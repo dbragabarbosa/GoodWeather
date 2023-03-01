d3dc92b3a9bdca2e58ae3e5aefc93fcb3f51f9a6
Daniel Braga <dbragabarbosa@gmail.com>
2023-03-01 11:15:17 -0300

    Configurando a TableView

---
+++ GoodWeather/Custom Cells/WeatherCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by Daniel Braga Barbosa on 01/03/23.
+//
+
+import Foundation
+
+import UIKit
+
+class WeatherCell: UITableViewCell
+{
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
