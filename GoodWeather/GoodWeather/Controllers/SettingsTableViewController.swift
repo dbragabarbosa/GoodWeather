3508383389da9c55a3e5df4c607c05b10c5ce118
Daniel Braga <dbragabarbosa@gmail.com>
2023-03-20 11:15:03 -0300

    Adicionando a tela de Settings

---
+++ GoodWeather/GoodWeather/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsTableViewController.swift
+//  GoodWeather
+//
+//  Created by Mohammad Azam on 3/4/21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+}
---
+++ GoodWeather/GoodWeather/View Models/WeatherListViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  WeatherListViewModel.swift
+//  GoodWeather
+//
+//  Created by Mohammad Azam on 3/4/21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+class WeatherListViewModel {
+    
+    private var weatherViewModels = [WeatherViewModel]()
+    
+    func addWeatherViewModel(_ vm: WeatherViewModel) {
+        weatherViewModels.append(vm)
+    }
+    
+    func numberOfRows(_ section: Int) -> Int {
+        return weatherViewModels.count
+    }
+    
+    func modelAt(_ index: Int) -> WeatherViewModel {
+        return weatherViewModels[index]
+    }
+}
+
+class WeatherViewModel {
+    
+    let weather: WeatherResponse
+    
+    init(weather: WeatherResponse) {
+        self.weather = weather
+    }
+    
+    var city: String {
+        return weather.name
+    }
+    
+    var temperature: Double {
+        return weather.main.temp 
+    }
+    
+}
+
+
